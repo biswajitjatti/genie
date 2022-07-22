ea607d9e5c8352bcc89a93851a00fcde22096312
Crimsonite <aantriksh.pandya@gmail.com>
2022-07-22 11:34:35 +0530

    Add files via upload

---
+++ Line.java
@@ -0,0 +1,6 @@
+import java.util.List;
+
+public class Line {
+  int lineNum;
+  List<Selection> selections;
+}
---
+++ Selection.java
@@ -0,0 +1,13 @@
+public class Selection {
+  String name;
+  int start;
+  int end;
+  String type;
+  String regex;
+  String description;
+
+  @Override
+  public String toString() {
+    return "Name:" + name + " start:" + start + " end:" + end;
+  }
+}
